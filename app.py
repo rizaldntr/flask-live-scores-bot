39a49a3781818c28ccd42798232c9c17cd2a331f
Rizal Diantoro <rizaldntr@gmail.com>
2018-06-17 20:47:43 +0700

    specify event message only for text and prefix /wc18

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, request, abort
 
 from linebot import (
@@ -37,9 +38,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if (event['message']['type'] != 'text' and
+            re.match(r'\/wc18\s.*', event['message']['text'])):
+        return
+
+    if event['message']['text'] == '/wc18 help':
+        reply_message = 'hola-hola'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=reply_message))
 
 
 if __name__ == "__main__":
