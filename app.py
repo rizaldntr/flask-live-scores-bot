f97358430c93b74729b3da789953258a30d3cec0
Rizal Diantoro <rizaldntr@gmail.com>
2018-06-18 12:09:01 +0700

    try to send flex message in barbar way

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import re
-import urllib.request
+import requests
 import json
 from flask import Flask, request, abort
 
@@ -14,9 +14,12 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+from utils import flex_today_matches_builder
+
 BASE_URL = 'https://world-cup-json.herokuapp.com/matches'
 TODAY_MATCHES = '/today'
 CURRENT_MATCH = '/current'
+LINE_API='https://api.line.me/v2/bot/message/push'
 
 app = Flask(__name__)
 
@@ -48,104 +51,36 @@ def handle_message(event):
             re.match(r'\/wc18\s.*', event.message.text) is None):
         return
 
-    reply_message = 'Mohon maaf perintah tidak dikenal'
-
     if event.message.text == '/wc18 help':
-        reply_message = 'hola-hola'
-
-    if event.message.text == '/wc18 dev':
-        reply_message = [
-            {
-                "type": "flex",
-                "altText": "This is a Flex Message",
-                "contents": {
-                    "type": "bubble",
-                    "body": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                        {
-                            "type": "box",
-                            "layout": "horizontal",
-                            "contents": [
-                            {
-                                "type": "image",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png"
-                            },
-                            {
-                                "type": "text",
-                                "text": "1 - 2",
-                                "gravity": "center",
-                                "align": "center",
-                                "size": "xxl"
-                            },
-                            {
-                                "type": "image",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png"
-                            }
-                            ]
-                        },
-                        {
-                            "type": "box",
-                            "layout": "horizontal",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "Korea Republic",
-                                "gravity": "center",
-                                "align": "center",
-                                "size": "sm",
-                                "wrap": True
-                            },
-                            {
-                                "type": "text",
-                                "text": "Time: 75'",
-                                "gravity": "center",
-                                "align": "center",
-                                "size": "sm"
-                            },
-                            {
-                                "type": "text",
-                                "text": "Mexico",
-                                "gravity": "center",
-                                "align": "center",
-                                "size": "sm",
-                                "wrap": True
-                            }
-                            ]
-                        },
-                        {
-                            "type": "separator",
-                            "margin": "lg"
-                        }]
-                    }    
-                }
-            }
-        ]
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Halo'))
 
     if event.message.text == '/wc18 today':
         todayURL = '{}{}'.format(BASE_URL, TODAY_MATCHES)
-        with urllib.request.urlopen(todayURL) as url:
-            reply_message = ''
-            datas = json.loads(url.read().decode())
-            for data in datas:
-                if data['time'] == None:
-                    time = (int(data['datetime'][11:-7]) + 7) % 24
-                    tmp = '{}:00'.format(time)
-                    tmp = tmp.center(44, ' ') + '\n'
-                    reply_message += tmp
-                else:
-                    reply_message += data['time'].center(44, ' ') + '\n'
-                tmp = '{} {} - {} {}'.format(
-                    data['home_team']['country'], data['home_team']['goals'],
-                    data['away_team']['country'], data['away_team']['goals']
-                )
-                reply_message += tmp.center(44, ' ') + '\n\n'
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message))
-
+        res = requests.get(todayURL)
+        datas = json.loads(res.content)
+        messages = []            
+        for data in datas:
+            if data['time'] is None:
+                time = (int(data['datetime'][11:-7]) + 7) % 24
+                time_str = '{}:00'.format(time)
+            else:
+                time_str = 'Time: {}\''.format(data['time'])
+            tmp = flex_today_matches_builder(
+                data['home_team']['country'], data['away_team']['country'],
+                data['home_team']['goals'], data['away_team']['goals'],
+                time_str
+            )
+            messages.append(tmp)
+        payload = {
+            'to': event.reply_token,
+            'messages': messages
+            }
+        headers = {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer {}'.format(line_bot_api)}
+        res = requests.post(LINE_API, data=payload, headers=headers)
 
 if __name__ == "__main__":
     app.run()
