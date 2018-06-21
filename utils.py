68860ec0493002c378429d0cba5ad9247625715b
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-06-22 06:13:12 +0700

    wip add goals scorer

--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+def build_goal_by_content(home, away):
+    home_scored_by = {}
+    away_scored_by = {}
+
+    for x in home:
+        # TODO
+    
+    for x in away:
+        # TODO
+
+    return home_scored_by, away_scored_by
+
 def flex_today_matches_builder(home_team, away_team, home_goals, away_goals, time, home_code, away_code):
     picture_image_uri='https://api.fifa.com/api/v1/picture/flags-fwc2018-4/'
     return {
@@ -62,6 +74,22 @@ def flex_today_matches_builder(home_team, away_team, home_goals, away_goals, tim
                     {
                         "type": "separator",
                         "margin": "lg"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": None #TODO: home goalie
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": None #TODO: away goalie
+                            }
+                        ]
                     }
                 ]
             }
