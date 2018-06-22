b2cc526a749ed535729392dceb7bce0ea4e88b6f
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-06-22 16:42:02 +0700

    change to one line string

--- app.py
+++ app.py
@@ -86,14 +86,7 @@ def handle_message(event):
         return
 
     if event.message.text == '/wc18 help':
-        msg = """
-            Format : '/wc18 <command>'
-            Available commands :
-            today -> display today's matches.
-            current -> display current match.
-            start live -> start live score notification
-            stop live -> stop live score notification
-        """
+        msg = "Format : '/wc18 <command>'\nAvailable commands :\ntoday -> display today's matches.\ncurrent -> display current match.\nstart live -> start live score notification\nstop live -> stop live score notification"
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=msg))
