2a6fe7a81794b4bb66c439972842bdccb1a0c13a
AdityaAryaHendrady <aryaditya.85@gmail.com>
2018-06-23 16:23:29 +0700

    Debugging for santuy

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text='Masuk santuy')
+                TextSendMessage(text='Masuk santuy ' + len(data))
             )
 
         return
